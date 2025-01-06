441c268d8d2dc7cb47181eb1af856bdfd7cd781e
Volume999 <Volume99917@gmail.com>
2025-01-06 14:26:37 +0100

    Data Generation Handler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -17,21 +18,62 @@ func handleHealthcheck(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(w).Encode(hc)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Unexpected Error", http.StatusInternalServerError)
 		return
 	}
 }
 
-func NewServer() http.Handler {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/healthcheck", handleHealthcheck)
-	return mux
+type Generator interface {
+	Generate(sz int) ([]byte, error)
 }
 
+type DataGenerationHandler struct {
+	sz  int
+	gen Generator
+}
+
+func NewDataGenerationHandler(sz int, gen Generator) (*DataGenerationHandler, error) {
+	return &DataGenerationHandler{sz: sz, gen: gen}, nil
+}
+
+func (h *DataGenerationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if h.gen == nil {
+		http.Error(w, "Generator is not set", http.StatusInternalServerError)
+		return
+	}
+	data, err := h.gen.Generate(h.sz)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error during data generation: %v", err), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(data)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error during data retrieval: %v", err), http.StatusInternalServerError)
+		return
+	}
+}
+
+func NewServer(gen Generator) (http.Handler, error) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthcheck", handleHealthcheck)
+	h, err := NewDataGenerationHandler(100, gen)
+	if err != nil {
+		return nil, err
+	}
+	mux.Handle("/data", h)
+	return mux, nil
+}
+
+// TODO: This should be in some example folder
 func run(l *log.Logger) {
-	s := NewServer()
+	s, err := NewServer(nil)
+	if err != nil {
+		l.Fatal("Failed to create server: ", err)
+		return
+	}
 	l.Println("Starting server on :8080")
-	err := http.ListenAndServe(":8080", s)
+	err = http.ListenAndServe(":8080", s)
 	if err != nil {
 		l.Fatal(err)
 	}
--- main_test.go
+++ main_test.go
@@ -2,10 +2,85 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+// TODO: Test with Dummy Data
+type DummyData struct {
+	SomeNum  int    `json:"someNum"`
+	SomeText string `json:"someText"`
+}
+type DummyGenerator struct{}
+
+type GenerateFunc func(sz int) ([]byte, error)
+
+func (f GenerateFunc) Generate(sz int) ([]byte, error) {
+	return f(sz)
+}
+
+func GenerateDummyData(sz int) ([]byte, error) {
+	data := make([]DummyData, sz)
+	for i := 0; i < sz; i++ {
+		data[i] = DummyData{SomeNum: i, SomeText: "SomeText"}
+	}
+	return json.Marshal(data)
+}
+
+func TestDataGenerationHandlerReturnsErrorWhenGeneratorIsNotSet(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/data", nil)
+	rr := httptest.NewRecorder()
+	handler, _ := NewDataGenerationHandler(100, nil)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusInternalServerError {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusInternalServerError)
+	}
+	if body := rr.Body.String(); body != "Generator is not set\n" {
+		t.Errorf("handler returned unexpected body: got %v want %v", body, "Generator is not set")
+	}
+}
+
+func TestDataGenerationHandlerReturnsErrorWhenDataGenerationFails(t *testing.T) {
+	failGen, _ := NewDataGenerationHandler(
+		100,
+		GenerateFunc(func(sz int) ([]byte, error) {
+			return nil, errors.New("some error")
+		}))
+	req, _ := http.NewRequest("GET", "/data", nil)
+	rr := httptest.NewRecorder()
+	failGen.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusInternalServerError {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusInternalServerError)
+	}
+	if body := rr.Body.String(); body != "Error during data generation: some error\n" {
+		t.Errorf("handler returned unexpected body: got %v want %v", body, "Error during data generation: some error")
+	}
+}
+
+func TestDataGenerationHandlerReturnsExpectedResult(t *testing.T) {
+	gen, _ := NewDataGenerationHandler(2, GenerateFunc(GenerateDummyData))
+	req, _ := http.NewRequest("GET", "/data", nil)
+	rr := httptest.NewRecorder()
+	gen.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+	want := []DummyData{{SomeNum: 0, SomeText: "SomeText"}, {SomeNum: 1, SomeText: "SomeText"}}
+	var got []DummyData
+	if err := json.Unmarshal(rr.Body.Bytes(), &got); err != nil {
+		t.Errorf("handler response could not be unmarshalled: %v", err)
+	}
+	if len(got) != len(want) {
+		t.Errorf("handler returned unexpected body: got %v want %v", got, want)
+	}
+	for i := range got {
+		if got[i] != want[i] {
+			t.Errorf("handler returned unexpected body: got %v want %v", got, want)
+		}
+	}
+}
 
 func TestHandleHealthcheckDoesNotReturnError(t *testing.T) {
 	req, err := http.NewRequest("GET", "/healthcheck", nil)
@@ -36,10 +111,10 @@ func TestHandleHealthcheckReturnsExpectedResult(t *testing.T) {
 	}
 }
 
-func TestNewServer(t *testing.T) {
-	s := NewServer()
+func TestNewServerRespondsToHealthcheck(t *testing.T) {
+	s, err := NewServer(nil)
 	if s == nil {
-		t.Errorf("NewServer returned an error")
+		t.Errorf("NewServer returned an error: %v", err)
 		return
 	}
 	req, err := http.NewRequest("GET", "/healthcheck", nil)
